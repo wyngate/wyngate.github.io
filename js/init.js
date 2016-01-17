24c50d177ee1b0d1db8f6573009208a3ee31655b
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2016-01-17 15:46:27 -0800

    minor updates

--- js/init.js
+++ js/init.js
@@ -11,7 +11,7 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			
+
 			// Global.
 				global: {
 					range: '*',
@@ -81,44 +81,44 @@
 		},
 		plugins: {
 			layers: {
-				
+
 				// Config.
 					config: {
 						transform: true
 					},
-				
-				// Navigation Panel.
-					navPanel: {
-						animation: 'pushX',
-						breakpoints: 'medium',
-						clickToHide: true,
-						height: '100%',
-						hidden: true,
-						html: '<div data-action="moveElement" data-args="nav"></div>',
-						orientation: 'vertical',
-						position: 'top-left',
-						side: 'left',
-						width: 250
-					},
 
-				// Navigation Button.
-					navButton: {
-						breakpoints: 'medium',
-						height: '4em',
-						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
-						position: 'top-left',
-						side: 'top',
-						width: '6em'
-					}
+				// // Navigation Panel.
+				// 	navPanel: {
+				// 		animation: 'pushX',
+				// 		breakpoints: 'medium',
+				// 		clickToHide: true,
+				// 		height: '100%',
+				// 		hidden: true,
+				// 		html: '<div data-action="moveElement" data-args="nav"></div>',
+				// 		orientation: 'vertical',
+				// 		position: 'top-left',
+				// 		side: 'left',
+				// 		width: 250
+				// 	},
+				//
+				// // Navigation Button.
+				// 	navButton: {
+				// 		breakpoints: 'medium',
+				// 		height: '4em',
+				// 		html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+				// 		position: 'top-left',
+				// 		side: 'top',
+				// 		width: '6em'
+				// 	}
 
 			}
 		}
 	});
 
 	$(function() {
-		
+
 		// jQuery ready stuff.
-		
+
 	});
 
 })(jQuery);
